8dedfcfbc4f886a88d48250304ecc26abbdf716f
Visalpou <povvisal001@gmail.com>
2023-07-19 12:06:20 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,3 +87,34 @@ $(function() {
             }
     });
 });
+
+const blurDivs = document.querySelectorAll('.blur-load');
+blurDivs.forEach(div => {
+    const img = div.querySelector("img")
+
+    function loaded(){
+        div.classList.add("loaded")
+    }
+
+    if(img.complete){
+        loaded();
+    }else{
+        img.addEventListener('load',loaded)
+    }
+})
+
+const blurImg = document.querySelectorAll('.blurred-img');
+blurImg.forEach(div => {
+    const img = div.querySelector("img")
+
+    function loaded(){
+        div.classList.add("loaded")
+    }
+
+    if(img.complete){
+        loaded();
+    }else{
+        img.addEventListener('load',loaded);
+        div.classList.remove("blurred-img");
+    }
+})
